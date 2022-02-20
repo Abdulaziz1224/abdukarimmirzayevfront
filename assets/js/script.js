4486b1c1375fbee2c87cdc20e1570cc6079e73c1
unknown <navoiy498@gmail.com>
2022-02-21 01:55:32 +0500

    dkjdsnkdjn

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,4 +137,4 @@ allImages.forEach((value) => {
 let spinnerWrapper = document.querySelector(".font_loader");
 setTimeout(() => {
   spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-}, 500);
+}, 1000);
