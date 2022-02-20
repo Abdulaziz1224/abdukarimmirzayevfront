49f78e2428d691b4e04414477ece4d6ff81ee434
unknown <navoiy498@gmail.com>
2022-02-21 01:22:32 +0500

    dkjdsnkdjn

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,15 +78,15 @@ Array.isArray(accar)
   : "";
 
 // show and hide accardion Function //
-window.addEventListener("load", () => {
-  if (ul.children[0].classList.length === 1) {
-    ul.children[0].classList.add("active");
-    ul.children[0].firstElementChild.lastElementChild.children[0].style.transform =
-      "rotate(0deg)";
+// window.addEventListener("load", () => {
+//   if (ul.children[0].classList.length === 1) {
+//     ul.children[0].classList.add("active");
+//     ul.children[0].firstElementChild.lastElementChild.children[0].style.transform =
+//       "rotate(0deg)";
 
-    ul.children[0].lastElementChild.style.height = "192px";
-  }
-});
+//     ul.children[0].lastElementChild.style.height = "192px";
+//   }
+// });
 
 let height = null;
 const showAccardion = (id) => {
@@ -134,7 +134,7 @@ allImages.forEach((value) => {
   };
 });
 
-let spinnerWrapper = document.querySelector(".font_loader");
-setTimeout(() => {
-  spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-}, 3000);
+// let spinnerWrapper = document.querySelector(".font_loader");
+// setTimeout(() => {
+//   spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+// }, 3000);
